8096dfe3a633d8cea2e147765144d807a4ba583a
Asif Rana <muhammadasif@Muhammads-MBP.station>
2022-03-22 22:08:35 +0000

    features component updated

--- components/Features.jsx
+++ components/Features.jsx
@@ -29,13 +29,13 @@ const Features = () => {
                 </div>
                 <div className=" rounded-3xl overflow-hidden relative">
                     <img className="w-full object-cover h-full" src="https://images.unsplash.com/photo-1588854337236-6889d631faa8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80" alt="" />
-                    <div className=" absolute right-0 bottom-0 p-6 rounded-tl-3xl text-white bg-orange-500 text-xl">
+                    <div className=" absolute right-0 bottom-0 md:p-6 p-4 rounded-tl-3xl text-white bg-orange-500 md:text-xl">
                         Luxurious Kitchen
                     </div>
                 </div>
                 <div className=" rounded-3xl overflow-hidden relative">
                     <img className="w-full object-cover h-full" src="https://images.unsplash.com/photo-1416331108676-a22ccb276e35?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2367&q=80" alt="" />
-                    <div className=" absolute right-0 bottom-0 p-6 rounded-tl-3xl text-white bg-orange-500 text-xl">
+                    <div className=" absolute right-0 bottom-0 md:p-6 p-4 rounded-tl-3xl text-white bg-orange-500 md:text-xl">
                         Swimming Pools
                     </div>
                 </div>
